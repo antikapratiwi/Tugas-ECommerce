a9ffc089c65cd607946c9982ddb4f1e265c4b37c
alwizzz <alwiza21@gmail.com>
2023-11-30 10:42:47 +0700

    create models

---
+++ app/Models/Standar.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Standar extends Model
+{
+    use HasFactory;
+}
