93aef2c9771fe2902492c2db49e1fef32dbc3131
antikapratiwi <antikapratiwi11@gmail.com>
2023-12-02 21:54:42 +0700

    add home page

---
+++ resources/views/about.blade.php
@@ -0,0 +1,15 @@
+<div class="box-about">
+
+    <div class="ellips-about">
+        <div class="circle-about">
+            <p class="font-about">
+            About
+            </p>
+        </div>
+        <p class="content-about">
+        Si AUdit merupakan website yang membantu perihal audit suatu lembaga atau perusahaan dengan harga yang murah, dapat membantu proses menjadi lebih mudah dan cepat. Dalam sistem ini hanya perlu melengkapi file dengan cara mengunggahnya yang dapat dilakukan kapanpun dan di manapun
+
+        </p>
+
+    </div>
+</div>
---
+++ resources/views/components/navbar.blade.php
@@ -0,0 +1,17 @@
+<div class="navbar">
+    <div class="navbar-menu">
+        <div class="icon-img">
+         <img src="{{ asset('assets/si_audit.png') }}"/>
+        </div>
+        <div class="navbar-main-menu">
+            <div><a class="home" href="">Home</a></div>
+            <div><a class="about" href="">About</a></div>
+            <div ><a class="our-service" href=""> Our Service</a></div>
+            <div ><a class="service-map" href="">Service Map</a></div>
+            <div ><a class="contact-us" href="">Contact Us</a></div>
+        </div>
+        <div class="button-sign-in">
+            <div class="sign-in">Sign In</div>
+        </div>
+    </div>
+</div>
