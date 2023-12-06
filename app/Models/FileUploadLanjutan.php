888cf8b1f01ccb701f78ccf4018b0a5202541e6e
alwizzz <alwiza21@gmail.com>
2023-12-06 15:20:11 +0700

    progress adding ORM relationship on models

--- app/Models/FileUploadLanjutan.php
+++ app/Models/FileUploadLanjutan.php
@@ -5,7 +5,16 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\HasOne;
+use Illuminate\Database\Eloquent\Relations\HasMany;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
+
 class FileUploadLanjutan extends Model
 {
     use HasFactory;
+
+    protected $guarded = [
+        'id'
+    ];
 }
